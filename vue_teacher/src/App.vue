a787d642cf312c001e7a0cb80667a7a3d28a294f
yuukyou-chan <1968159353@qq.com>
2021-09-22 15:14:24 +0800

    初始化项目结构

--- vue_teacher/src/App.vue
+++ vue_teacher/src/App.vue
@@ -1,32 +1,15 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <h1>天津商业大学信息工程学院党员培育系统</h1>
     <router-view/>
   </div>
 </template>
 
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
--- vue_teacher/src/main.js
+++ vue_teacher/src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import './plugins/element.js'
+// 加载全局样式文件
+import './styles/index.less'
 
 Vue.config.productionTip = false
 
